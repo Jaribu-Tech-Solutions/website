3e59f071f371b54456246b257d6c5cb96eba225b
Mati Joseph <mati.joseph.dev@gmail.com>
2026-06-11 10:11:46 +0300

    fix  remove SamplePage import

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,7 +6,6 @@ import Navbar from "./components/Navbar";
 import Portfolio from "./components/Portfolio";
 import Pricing from "./components/Pricing";
 import Product from "./components/Product";
-import SamplePage from "./components/SamplePage";
 import Services from "./components/Services";
 import Team from "./components/Team";
 import TechStack from "./components/TechStack";
@@ -21,7 +20,6 @@ function App() {
       <HowWeWork />
       <Portfolio />
       <Product />
-      <SamplePage />
       <Pricing />
       <Team />
       <Contact />
